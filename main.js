1417afb30ec557bc84dbfd6020fd43f521f7b0eb
fredrikparkell <fredrikparkell5@gmail.com>
2021-05-09 19:00:40 +0200

    Restructure

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ btn.addEventListener("click", async function () {
     const query = "sort=date-taken-asc&per_page=10&format=json&nojsoncallback=1"; // nojsoncallback=1
     const flickrURL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&${apiKey}&${text}&${query}`; // api.flickr.com || www.flickr.com
     
-    console.log(flickrURL);
+    //console.log(flickrURL);
 
-    fetch(flickrURL)
+    talkToFlickr(flickrURL);
+
+    /*fetch(flickrURL)
     .then(response => response.json())
     .then(data => {
         
@@ -20,60 +22,57 @@ btn.addEventListener("click", async function () {
         console.log(myArr.photos.photo);
         console.log(myArr.photos.photo[0]);
         console.log(myArr.photos.photo[0].id);
-        console.log(myArr.stat);*/
+        console.log(myArr.stat);
 
         //const imageSection = document.getElementById("main");
 
         for (let i = 0; i < data.photos.photo.length; i++){
 
-            let farmId = data.photos.photo[i].farm;
+            //let farmId = data.photos.photo[i].farm;
             let serverId = data.photos.photo[i].server;
             let photoId = data.photos.photo[i].id;
             let secret = data.photos.photo[i].secret;
-            let imgURL = `https://live.staticflickr.com/${serverId}/${photoId}_${secret}_m.jpg`; // [mstzb]   https://farm${farmId}.staticflickr.com/${serverId}/${photoId}_${secret}_m.jpg
+            let imgURL = `https://live.staticflickr.com/${serverId}/${photoId}_${secret}_s.jpg`; // [mstzb]   https://farm${farmId}.staticflickr.com/${serverId}/${photoId}_${secret}_m.jpg
             // https://live.staticflickr.com/${serverId}/${photoId}_${secret}_m.jpg
             createImage(imgURL);
-            //let img = document.createElement("img")
-            //img.setAttribute("src", imgURL);
-            //imageSection.appendChild(img);
         }
-    });
-
-    function createImage(imgURL) {
-        console.log("hrj");
-        const imageSection = document.getElementById("mainImgArea");
-        let img = document.createElement('img');
-        console.log("here we go");
-        img.setAttribute("src", imgURL);
-        img.src = imgURL;
-        console.log(img.src);
-        imageSection.innerHTML = "<img src=" + "&#34" + imgURL + "&#34" + "></img>";
-        imageSection.appendChild(img); // getElementById("main")
-        console.log("im going crazy");
-    }
-
-    //console.log(responseData);
-
-    /*const response = await fetch(flickrURL);
-    console.log(response);
-    console.log(response.url);
-    console.log(response.ok);
-    const result = await response.json();
-    console.log(result.results);
-
-    console.log(flickrURL);
-    
-    console.log(information)
-    console.log(information.photo)
-    const imagelist = document.querySelector(".uList")
-
-     for (let i = 0; i< information.photo.length; i++){
-
-        const listitem = document.createElement("li")
-        listitem.innerText = "hello"
-        const img = document.createElement("img")
-        imagelist.append(listitem)
-        //listitem.append(img)
-        
-     }*/
+    }).catch(err => {
+        console.log(err);
+    });*/
 })
+
+async function talkToFlickr(flickrURL) {
+    try {
+        console.log(flickrURL);
+        const response = await fetch(flickrURL);
+        console.log("response recieved")
+        const data = await response.json();
+        console.log("data recieved")
+        loopData(data);
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+async function loopData(data) {
+    for (let i = 0; i < data.photos.photo.length; i++){
+        //let farmId = data.photos.photo[i].farm;
+        let serverId = data.photos.photo[i].server;
+        let photoId = data.photos.photo[i].id;
+        let secret = data.photos.photo[i].secret;
+        let imgURL = `https://live.staticflickr.com/${serverId}/${photoId}_${secret}_s.jpg`; // [mstzb]   https://farm${farmId}.staticflickr.com/${serverId}/${photoId}_${secret}_m.jpg
+        // https://live.staticflickr.com/${serverId}/${photoId}_${secret}_m.jpg
+        createImage(imgURL);
+    }
+}
+
+async function createImage(imgURL) {
+    let imageSection = document.getElementById('main');
+    console.log("here we go");
+    let imgTag = document.createElement('img');
+    imgTag.src = imgURL;
+    //imgTag.setAttribute("src", imgURL)
+    console.log(imgTag.src);
+    imageSection.appendChild(imgTag);
+    console.log("im going crazy");
+}
