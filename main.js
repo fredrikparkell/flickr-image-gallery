da23541ecb876c0d479e773e9768efddab77c5ac
fredrikparkell <fredrikparkell5@gmail.com>
2021-05-10 15:43:50 +0200

    lightbox addition

--- main.js
+++ main.js
@@ -1,77 +1,76 @@
-const btn = document.querySelector(".searchBtn");
-console.log(btn)
+// GLOBAL SCOPE VARIABLES //
 
-btn.addEventListener("click", async function () {
+let currentPage = 1;
+let totalPages = 0;
+const imageSection = document.getElementById('mainImgArea');
 
-    const apiKey = "api_key=0beca48521ee0ee70915815ea49063f4";
-    const searchText = document.getElementById("searchText").value;
-    const text = "text=" + searchText;
-    const query = "sort=date-taken-asc&per_page=20&format=json&nojsoncallback=1"; // nojsoncallback=1
-    const flickrURL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&${apiKey}&${text}&${query}`; // api.flickr.com || www.flickr.com
+// GLOBAL SCOPE VARIABLES //
 
-    console.log(searchText)
-    //console.log(flickrURL);
-
-    talkToFlickr(flickrURL);
-
-    /*fetch(flickrURL)
-    .then(response => response.json())
-    .then(data => {
-        
-        /*const myArr = data;
-        console.log(myArr);
-        console.log(myArr.photos);
-        console.log(myArr.photos.photo);
-        console.log(myArr.photos.photo[0]);
-        console.log(myArr.photos.photo[0].id);
-        console.log(myArr.stat);
-
-        //const imageSection = document.getElementById("main");
-
-        for (let i = 0; i < data.photos.photo.length; i++){
-
-            //let farmId = data.photos.photo[i].farm;
-            let serverId = data.photos.photo[i].server;
-            let photoId = data.photos.photo[i].id;
-            let secret = data.photos.photo[i].secret;
-            let imgURL = `https://live.staticflickr.com/${serverId}/${photoId}_${secret}_s.jpg`; // [mstzb]   https://farm${farmId}.staticflickr.com/${serverId}/${photoId}_${secret}_m.jpg
-            // https://live.staticflickr.com/${serverId}/${photoId}_${secret}_m.jpg
-            createImage(imgURL);
-        }
-    }).catch(err => {
-        console.log(err);
-    });*/
+document.querySelector(".searchBtn").addEventListener("click", async function () {
+    currentPage = 1;
+    loadImages();
 })
 
+function loadImages() {
+    imageSection.innerHTML = "";
+    const apiKey = "api_key=0beca48521ee0ee70915815ea49063f4";
+    const searchText = document.getElementById("searchText").value;
+    if (searchText == "") {location.reload();}
+    else {
+    const text = "text=" + searchText;
+    const query = "sort=date-taken-asc&per_page=20&format=json&nojsoncallback=1" + `&page=${currentPage}`; // nojsoncallback=1 // per_page=20
+    const flickrURL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&${apiKey}&${text}&${query}`;
+    talkToFlickr(flickrURL);
+    }
+}
+
 async function talkToFlickr(flickrURL) {
-        console.log(flickrURL);
+        console.log(flickrURL) // ska ta bort
         const response = await fetch(flickrURL);
-        console.log("response recieved")
         const data = await response.json();
-        console.log("data recieved")
-        console.log(data)
+        totalPages = data.photos.pages;
         loopData(data);
 }
 
- function loopData(data) {
+function loopData(data) {
     for (let i = 0; i < data.photos.photo.length; i++){
-        //let farmId = data.photos.photo[i].farm;
+        //let farmId = data.photos.photo[i].farm; // https://live.staticflickr.com/${serverId}/${photoId}_${secret}_m.jpg
         let serverId = data.photos.photo[i].server;
         let photoId = data.photos.photo[i].id;
         let secret = data.photos.photo[i].secret;
-        let imgURL = `https://live.staticflickr.com/${serverId}/${photoId}_${secret}_q.jpg`; // [mstzb]   https://farm${farmId}.staticflickr.com/${serverId}/${photoId}_${secret}_m.jpg
-        // https://live.staticflickr.com/${serverId}/${photoId}_${secret}_m.jpg
+        let imgURL = `https://live.staticflickr.com/${serverId}/${photoId}_${secret}_q.jpg`; // https://farm${farmId}.staticflickr.com/${serverId}/${photoId}_${secret}_m.jpg
         createImage(imgURL);
     }
 }
 
- function createImage(imgURL) {
-    let imageSection = document.getElementById('mainImgArea');
-    console.log("here we go");
-    let imgTag = document.createElement('img');
-    imgTag.src = imgURL;
-    //imgTag.setAttribute("src", imgURL)
-    console.log(imgTag.src);
-    imageSection.appendChild(imgTag);
-    console.log("im going crazy");
+function createImage(imgURL) {
+    let newImg = document.createElement('img');
+    newImg.src = imgURL;
+    imageSection.appendChild(newImg);
+
+    newImg.addEventListener("click", async function () { // inte implementerat
+        document.getElementById('lightbox').style.display = "block";
+        let bigSrc = newImg.src.replace('q.jpg', 'z.jpg');
+        document.getElementById('lightboxImg').setAttribute("src", bigSrc);
+    })
 }
+
+document.getElementById('lightbox').addEventListener("click", async function () {
+    document.getElementById('lightbox').style.display = "none";
+})
+
+document.getElementById('imgBtnPrev').addEventListener("click", async function () {
+    if (currentPage > 1){
+        currentPage--;
+        loadImages();
+    }
+    // else some felmeddelande
+})
+
+document.getElementById('imgBtnNext').addEventListener("click", async function () {
+    if (currentPage < totalPages){
+        currentPage++;
+        loadImages();
+    }
+    // else some felmeddelande
+})
