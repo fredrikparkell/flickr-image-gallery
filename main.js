82bdda452084b8b7c245f10a2e6124f97b706303
fredrikparkell <fredrikparkell5@gmail.com>
2021-05-10 16:25:33 +0200

    added search results and page counter

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // GLOBAL SCOPE VARIABLES //
 
-let currentPage = 1;
-let totalPages = 0;
-const imageSection = document.getElementById('mainImgArea');
+    let currentPage = 1;
+    let totalPages = 0;
+    const imageSection = document.getElementById('mainImgArea');
 
 // GLOBAL SCOPE VARIABLES //
 
@@ -29,7 +29,12 @@ async function talkToFlickr(flickrURL) {
         const response = await fetch(flickrURL);
         const data = await response.json();
         totalPages = data.photos.pages;
-        loopData(data);
+        document.getElementById('searchResults').innerText = `Found ${data.photos.total} images based on your search results`;
+        if (totalPages == 0) {document.getElementById('pageCounter').innerText = `0 of ${totalPages}`;}
+        else {
+            document.getElementById('pageCounter').innerText = `${currentPage} of ${totalPages}`;
+            loopData(data);
+        }
 }
 
 function loopData(data) {
@@ -39,18 +44,20 @@ function loopData(data) {
         let photoId = data.photos.photo[i].id;
         let secret = data.photos.photo[i].secret;
         let imgURL = `https://live.staticflickr.com/${serverId}/${photoId}_${secret}_q.jpg`; // https://farm${farmId}.staticflickr.com/${serverId}/${photoId}_${secret}_m.jpg
-        createImage(imgURL);
+        createImage(imgURL, data.photos.photo[i].title);
     }
 }
 
-function createImage(imgURL) {
+function createImage(imgURL, imgTitle) {
     let newImg = document.createElement('img');
     newImg.src = imgURL;
+    newImg.title = imgTitle;
     imageSection.appendChild(newImg);
 
-    newImg.addEventListener("click", async function () { // inte implementerat
+    newImg.addEventListener("click", async function () {
         document.getElementById('lightbox').style.display = "block";
         let bigSrc = newImg.src.replace('q.jpg', 'z.jpg');
+        document.getElementById('lightboxPtag').innerText = newImg.title;
         document.getElementById('lightboxImg').setAttribute("src", bigSrc);
     })
 }
@@ -59,7 +66,7 @@ document.getElementById('lightbox').addEventListener("click", async function () 
     document.getElementById('lightbox').style.display = "none";
 })
 
-document.getElementById('imgBtnPrev').addEventListener("click", async function () {
+document.getElementById('imgBtnPrev').addEventListener("click", async function () { // pageCounter
     if (currentPage > 1){
         currentPage--;
         loadImages();
